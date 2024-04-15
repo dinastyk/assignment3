c70c291e0219220ac0c4a7586317c1753a150459
dinastyk <116314638+dinastyk@users.noreply.github.com>
2024-04-14 21:13:07 -0400

    final

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   constructor() {  // Create and initialize state
     super(); 
     this.state = {
-      accountBalance: 1234567.89,
+      accountBalance: 0,
       creditList: [],
       debitList: [],
       currentUser: {
@@ -35,6 +35,59 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
+updatebalance = (updatedbalance) => {
+  this.setState({
+  accountBalance: updatedbalance
+  });
+}
+
+  updatecredit = (updatedcredit) => {
+    this.setState({
+      creditList:[...this.state.creditList, updatedcredit]
+    })
+  }
+  
+//calculates the sum
+  accountBalancesums = () => {
+    const creditsum = this.state.creditList.reduce((creditsum, credit) => creditsum + credit.amount, 0);
+    const debitsum = this.state.debitList.reduce((debitsum, debit) => debitsum + debit.amount, 0);
+    return creditsum - debitsum;
+  }
+
+  // Update balance sum when "Credit" or "Debit" button is pressed on
+  Creditadd = (credit) => {
+    let updatedbalance = this.state.accountBalance + credit.amount;
+    this.setState({
+      accountBalance: updatedbalance
+    });
+  }
+
+  debitadd = (debit) => {
+    let updatedbalance = this.state.accountBalance - debit.amount;
+    this.setState({
+      accountBalance: updatedbalance
+    });
+  }
+
+//using api for credit and debit
+  componentDidMount() {
+    fetch('https://johnnylaicode.github.io/api/credits.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({creditList: data}, () => {
+          this.accountBalancesums()})
+      }).then(() => {
+        fetch('https://johnnylaicode.github.io/api/debits.json')
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({debitList: data}, () => {
+            this.accountBalancesums()})
+          })
+      });
+
+
+  }
+
   // Create Routes and React elements to be rendered using React components
   render() {  
     // Create React elements and pass input props to components
@@ -43,12 +96,18 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} />) 
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
+    const CreditsComponent = () => (<Credits credits={this.state.creditList} 
+    balance={this.state.accountBalance}
+    updatebalance={this.updatebalance}
+    updatecredit = {this.updatecredit}/>) 
+
+    const DebitsComponent = () => (<Debits debits={this.state.debitList} 
+    accountBalance ={this.state.accountBalance}/>) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
+    // //bank-of-react-starter-code"
     return (
-      <Router basename="/bank-of-react-starter-code">
+      <Router basename="/assignment3"> 
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
